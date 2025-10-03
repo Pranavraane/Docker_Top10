8a1ae2ae438dcccff4e578e1cf8d04dc0dcb5600
Pranav  Rane <92846251+Pranavraane@users.noreply.github.com>
2025-10-03 15:37:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
 from flask import Flask, request, render_template_string
-import subprocess
 
 app = Flask(__name__)
 
-# Existing vulnerable command execution route (RCE)
-@app.route('/run_command', methods=['POST'])
-def run_command():
-    cmd = request.form.get('cmd', '')
-    output = subprocess.getoutput(cmd)
-    return f"<pre>{output}</pre>"
-
-# New vulnerable SSTI route (D03 exploit)
 @app.route('/ssti')
 def ssti():
     name = request.args.get('name', 'User')
-    # Unsafe rendering of user input in template string - vulnerable to SSTI
     template = f"Hello {name}!"
     return render_template_string(template)
 
