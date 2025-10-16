6a23215dfb25b247f2ab569921123bc63d6d80bb
Pranav  Rane <92846251+Pranavraane@users.noreply.github.com>
2025-10-16 15:41:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask, request, render_template_string, render_template
+from flask import Flask, request
+import jinja2
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    name = request.args.get("name", "User")
-    template = f"Hello {name}!"
-    return render_template_string(template)
-    #patch: remove above 2 lines and add below one line, index.html file is recommented
-    #return render_template("index.html", name=name)
+def index():
+    return '''
+    <h2>OWASP D05: Maintain Security Contexts</h2>
+    <form method="POST" action="/vuln">
+        <input type="text" name="payload" placeholder="Enter payload">
+        <input type="submit" value="Run">
+    </form>
+    '''
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/vuln', methods=['POST'])
+def vuln():
+    data = request.form.get('payload')
+    # Unescaped user input = SSTI vulnerability
+    template = jinja2.Template(data)
+    return template.render()
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
