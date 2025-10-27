9d12d8b804526af4542574aea9445a3f034e6315
Pranav  Rane <92846251+Pranavraane@users.noreply.github.com>
2025-10-27 11:53:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, request
-import subprocess
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
-@app.route('/run_command', methods=['POST'])
-def run_command():
-    cmd = request.form.get('cmd', '')
-    # WARNING: Unsafe eval of user input -- rce vulnerability
-    output = subprocess.getoutput(cmd)
-    return f"<pre>{output}</pre>"
+@app.route('/')
+def index():
+    return '''
+    <h1>D07 Vulnerable Flask App</h1>
+    <form method="post" action="/greet">
+      Name: <input type="text" name="name">
+      <input type="submit" value="Greet Me">
+    </form>
+    '''
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/greet', methods=['POST'])
+def greet():
+    name = request.form.get('name')
+    # VULNERABLE: direct user input to template engine!
+    template = f"Hello {name}!"
+    return render_template_string(template)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
